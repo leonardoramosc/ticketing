dbab3cba4251ebe6e748d8b3cb7be938ae13da90
Leonardo Ramos <leorrc96@gmail.com>
2022-01-23 10:53:31 -0300

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import 'express-async-errors';
+
+import mongoose from 'mongoose';
+
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.use('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('DB connection succesfull');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+};
+
+start();
